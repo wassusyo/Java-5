c61927cbfb7b07d58aa00f8c2a25bc8da0b3d2d0
wassusyo <wassusyo1505@gmail.com>
2025-02-28 16:08:00 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -3,37 +3,31 @@ class Train {
     private String name;
     private String time;
 
-    // Конструктор с тремя параметрами
     public Train(String number, String name, String time) {
-        this(number, name); // Вызов конструктора с двумя параметрами
+        this(number, name); 
         this.time = time;
     }
 
-    // Конструктор с двумя параметрами
     public Train(String number, String name) {
         this.number = number;
         this.name = name;
-        this.time = "не указано"; // Инициализация по умолчанию
+        this.time = "не указано"; 
     }
 
-    // Конструктор без параметров
     public Train() {
         this.number = "не указан";
         this.name = "не указан";
-        this.time = "не указано"; // Инициализация по умолчанию
+        this.time = "не указано";
     }
 
-    // Метод receiveTrain
     public void receiveTrain() {
         System.out.println("Поезд рейса " + name);
     }
 
-    // Метод getNumber
     public String getNumber() {
         return number;
     }
 
-    // Метод для вывода информации о поезде
     public void displayInfo() {
         System.out.println("Номер поезда: " + number + ", Название: " + name + ", Время: " + time);
     }
@@ -41,17 +35,14 @@ class Train {
 
 public class Main {
     public static void main(String[] args) {
-        // Создание трех экземпляров класса Train
         Train train1 = new Train("001", "Экспресс", "10:00");
         Train train2 = new Train("002", "Скорый", "12:30");
         Train train3 = new Train("003", "Пассажирский");
-
-        // Вывод значений переменных на консоль
+        
         train1.displayInfo();
         train2.displayInfo();
         train3.displayInfo();
 
-        // Вызов методов для каждого объекта
         train1.receiveTrain();
         train2.receiveTrain();
         train3.receiveTrain();
